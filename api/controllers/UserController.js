231b8f1b3e7f03006bd1cf511df37c8062203beb
Santiago <santiago.pereztorre@hotmail.com>
2015-04-26 12:47:32 -0300

    Add user controller and actions

---
+++ api/controllers/UserController.js
@@ -0,0 +1,69 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+	new: function (req, res) {
+    return res.view();
+  },
+
+  create: function (req, res) {
+  	var params = req.allParams();
+    var newUser = {
+      username: params['username'],
+      email: params['email'],
+      password: params['password']
+    }
+    User.create(newUser, function (err, user) {
+      if (err) return res.redirect('/user/new');
+      return res.redirect('/user/show/' + user.id);
+    });
+  },
+
+  show: function (req, res) {
+    User.findOne(req.params["id"], function (err, user) {
+      if (err) res.negotiate(err);
+      return res.view({user: user});
+    });
+  },
+
+  index: function (req, res) {
+    User.find(function (err, users) {
+      if (err) res.negotiate(err);
+			return res.view({users: users});
+    });
+  },
+
+  edit: function (req, res) {
+    User.findOne(req.params["id"], function (err, user) {
+      if (err) res.negotiate(err);
+      return res.view({user: user});
+    });
+  },
+
+  update: function (req, res) {
+    var params = req.allParams();
+    var userID = req.params["id"];
+    var updUser = {
+      username: params['username'],
+      email: params['email']
+    }
+    User.update(req.params["id"], updUser, function (err) {
+      if (err) return res.redirect('/user/edit/' + userID);
+      return res.redirect('/user/show/' + userID);
+    });
+  },
+
+  destroy: function (req, res) {
+    User.destroy(req.params["id"], function (err) {
+      if (err) return res.negotiate(err);
+      return res.redirect('/users');
+    });
+  }
+
+};
+
